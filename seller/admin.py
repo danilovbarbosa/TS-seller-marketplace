5385f5eaf1db66bc16bb3929acf0a6c04b751e40
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 13:39:43 -0300

    refactor(seller/admin): adicionada class SellerAdmin e AddressAdmin.

--- seller/admin.py
+++ seller/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Address, Seller
 # Register your models here.
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
