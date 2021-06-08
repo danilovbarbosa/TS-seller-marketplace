d8a69a86e578b36d3a095a3eb82bd369a10c553b
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 13:29:59 -0300

    refactor(seller/models): adicionada class Address.

--- seller/models.py
+++ seller/models.py
@@ -8,3 +8,17 @@ class Seller(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Address(models.Model):
+    number = models.CharField(max_length=5)
+    street = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.street} - {self.district} - {self.number}"
