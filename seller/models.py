15e82f31db4ff47e927e8db464fc5cbdf284be0b
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 13:41:56 -0300

    refactor(seller/models): adiconados atributos na class Seller.

--- seller/models.py
+++ seller/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Seller(models.Model):
     name = models.CharField(max_length=100)
     document = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+
 
     def __str__(self):
         return self.name
