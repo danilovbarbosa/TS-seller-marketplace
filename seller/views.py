f3107b1986792b651895ef8aa60ec431e073f496
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 15:05:29 -0300

    refactor(templates/seller): adicionados arquivos index e base na pasta seller.

--- seller/views.py
+++ seller/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     content = {}
-    return render(request, 'index.html', context=content)
+    return render(request, 'seller/index.html', context=content)
