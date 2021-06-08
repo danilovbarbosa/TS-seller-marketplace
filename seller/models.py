a14d8a3487fedf1392101ea756b0cbcb8f1a3b5d
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 13:23:56 -0300

    refactor(seller/models): adicionado __str__ em Seller.

--- seller/models.py
+++ seller/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Seller(models.Model):
     name = models.CharField(max_length=100)
     document = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
