93da369fab9b1f6efccee298e63fdd110f34f14a
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 11:43:06 -0300

    env(scripts): adicionado o arquivo run.sh.

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# bash -f ./scripts/run.sh
+
+python manage.py showmigrations
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
+#python manage.py collectstatic
+
+python manage.py runserver 0.0.0.0:8000
