d3b3e9c6f3263758213a69d1ea639d0926b3b2ed
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 16:24:56 -0300

    feat(templates/seller/index): tabela para read seller.

--- seller/views.py
+++ seller/views.py
@@ -1,11 +1,17 @@
+from seller.models import Seller
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    content = {}
-    return render(request, 'seller/index.html', context=content)
+    list_sellers = Seller.objects.all()
+    
+    context = {
+        'list_sellers' : list_sellers
+    }
+    
+    return render(request, 'seller/index.html', context=context)
 
 
 def create(request):
-    content = {}
-    return render(request, 'seller/new_seller.html', context=content)
+    context = {}
+    return render(request, 'seller/new_seller.html', context=context)
