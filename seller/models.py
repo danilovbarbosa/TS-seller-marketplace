8fadf3ddecdec5c70387078f6529b6d001a3d550
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 13:21:55 -0300

    feat(seller/models): adicionado class Seller.

--- seller/models.py
+++ seller/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    document = models.CharField(max_length=20)
