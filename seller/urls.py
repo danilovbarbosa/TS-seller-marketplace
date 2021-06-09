5b23fdf1447aee4c2141693afa78c45155ae5aed
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-09 06:16:16 -0300

    feat(seller/views): adicionado SellerDetailView.

--- seller/urls.py
+++ seller/urls.py
@@ -3,7 +3,8 @@ from .views import (
     SellerListView,
     SellerCreateView,
     SellerUpdateView,
-    SellerDeleteView
+    SellerDeleteView,
+    SellerDetailView
 )
 
 
@@ -11,8 +12,6 @@ urlpatterns = [
     path('', SellerListView.as_view(), name='list'),
     path('create/', SellerCreateView.as_view(), name='create'),
     path('<pk>/update/', SellerUpdateView.as_view(), name='update'),
-
-    # <pk> is identification for id field,
-    # slug can also be used
     path('<pk>/delete/', SellerDeleteView.as_view(), name='delete'),
+    path('<pk>/delete/', SellerDetailView.as_view(), name='detail'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from django.urls.base import reverse_lazy
-
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
@@ -14,7 +11,6 @@ class SellerListView(ListView):
 
 
 class SellerCreateView(CreateView):
-    # template_name = 'seller/seller_form.html'
     model = Seller
     fields = ['name', 'document', 'phone_number', 'email']
     
@@ -43,10 +39,3 @@ class SellerDeleteView(DeleteView):
 class SellerDetailView(DetailView):
     
     model = Seller
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['seller_list'] = Seller.objects.all()
-        return context
