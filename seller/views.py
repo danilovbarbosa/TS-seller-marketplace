9533a797dff490e9f4553419347084c705133c22
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-08 21:25:33 -0300

    refactor(seller/views): retirando comentários.

--- seller/views.py
+++ seller/views.py
@@ -7,8 +7,6 @@ from django.views.generic.list import ListView
 
 class SellerListView(ListView):
     model = Seller
-    # queryset = Seller.objects.all()
-    # context_object_name = 'seller_list'
 
 
 class SellerCreateView(CreateView):
@@ -16,21 +14,3 @@ class SellerCreateView(CreateView):
     model = Seller
     fields = ['name', 'document', 'phone_number', 'email']
     
-    # def form_valid(self):
-    #     return super().form_valid(form)
-
-
-
-# def index(request):
-#     list_sellers = Seller.objects.all()
-    
-#     context = {
-#         'list_sellers' : list_sellers
-#     }
-    
-#     return render(request, 'seller/seller_list.html', context=context)
-
-
-# def create(request):
-#     context = {}
-#     return render(request, 'seller/new_seller.html', context=context)
