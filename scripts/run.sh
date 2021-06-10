4d097275fd4c80a4658899357637de9a91dfc5c6
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-10 16:54:19 -0300

    feat(run): adicionado linha: python manage.py check.

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,8 @@ python manage.py makemigrations
 
 python manage.py migrate
 
+#python manage.py check
+
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 
 #python manage.py collectstatic
