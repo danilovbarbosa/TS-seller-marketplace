3b44c7385c3d5e04d609f6fcd35de93e58b1f134
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-10 13:18:14 -0300

    test(seller/tests/test_models): foram adicionado teste para verificar inserção de dados nas tabelas Seller, Address e Contact.

--- seller/tests/test_models.py
+++ seller/tests/test_models.py
@@ -4,18 +4,12 @@ from seller.models import Seller, Address, Contact
 
 pytestmark = pytest.mark.django_db
 
-# class TestUsers:
-#     pytestmark = pytest.mark.django_db
-    
-#     def test_my_user(self):
-#         me = User.objects.get(username='admin')
-#         assert me.is_superuser
 
 @pytest.mark.django_db
 class TestSeller:
     pytestmark = pytest.mark.django_db
 
-    def test_um_novo_seller_com_todos_atributos_entao_deve_estar_no_bd(self):
+    def test_quando_um_novo_seller_com_todos_atributos_for_adicionado_no_bd_entao_a_tabela_deve_estar_com_mais_uma_linha(self):
         len_before = Seller.objects.all().count()
         Seller.objects.create(name="test", document="test", phone_number="test", email="test@test.com")
         
@@ -29,5 +23,36 @@ class TestSeller:
         Seller.objects.create(name="test", document="test", phone_number="test", email="test@test.com")
         
         len_after = Seller.objects.all().count()
+
+        assert len_before < len_after
+
+
+@pytest.mark.django_db
+class TestAddress:
+    pytestmark = pytest.mark.django_db
+
+    def test_quando_um_novo_address_com_todos_atributos_for_adicionado_no_bd_entao_a_tabela_deve_estar_com_mais_uma_linha(self):
+        len_before = Address.objects.all().count()
+        
+        seller = Seller.objects.create(name="test", document="test", phone_number="test", email="test@test.com")
+        Address.objects.create(number="test", street="test", district="test", city="test", state="test", zip_code="test", seller=seller)
+        
+        len_after = Address.objects.all().count()
+
+        assert len_before < len_after
+
+
+
+@pytest.mark.django_db
+class TestContact:
+    pytestmark = pytest.mark.django_db
+
+    def test_quando_um_novo_contact_com_todos_atributos_for_adicionado_no_bd_entao_a_tabela_deve_estar_com_mais_uma_linha(self):
+        len_before = Contact.objects.all().count()
+        
+        seller = Seller.objects.create(name="test", document="test", phone_number="test", email="test@test.com")
+        Contact.objects.create(number="test", seller=seller)
+        
+        len_after = Contact.objects.all().count()
 
         assert len_before < len_after
